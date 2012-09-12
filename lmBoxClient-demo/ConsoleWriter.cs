233cf8c3a74f7bee066664b326f90ff00aacb188
Konstantin Korotkov <konstantin.korotkov@labs64.de>
2012-09-12 02:05:50 +0200

    Added new files that were missed in the previous commit

---
+++ lmBoxClient-demo/ConsoleWriter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using lmBoxClient.Entities;
+
+namespace lmBoxClient
+{
+    class ConsoleWriter
+    {
+        public static void WriteEntity<T>(String msg, T entity) where T : IEntity
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine(entity.ToString());
+            Console.WriteLine("");
+        }
+
+        public static void WriteMsg(String msg)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine("");
+        }
+
+        public static void WriteList<T>(String msg, List<T> entitiesList) where T : IEntity
+        {
+            Console.WriteLine(msg);
+            foreach (IEntity entity in entitiesList)
+            {
+                Console.WriteLine(entity.ToString());
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ lmBoxClient/Utilities.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using lmBoxClient.Entities;
+using System.Globalization;
+
+namespace lmBoxClient.RestController
+{
+    static class Utilities
+    {
+        public static NumberFormatInfo lmBoxNumberFormat { get; private set; }
+
+        static Utilities()
+        {
+            lmBoxNumberFormat = new NumberFormatInfo();
+            lmBoxNumberFormat.NumberDecimalSeparator = ".";
+        }
+
+        public static Dictionary<String, String> forceCascadeToDict(Boolean forceCascade)
+        {
+            Dictionary<String, String> dict = new Dictionary<String, String>();
+            dict[Constants.CASCADE] = Convert.ToString(forceCascade).ToLower();
+            return dict;
+        }
+
+        public static Boolean CheckedParseBoolean(String source, String propertyName)
+        {
+            Boolean val;
+            if (Boolean.TryParse(source, out val))
+            {
+                return val;
+            }
+            else
+            {
+                throw new LmBoxException(String.Format("Expected value representing boolean for property '{0}', got '{1}'", propertyName, source));
+            }
+        }
+
+        public static Decimal CheckedParseDecimal(String source, String propertyName)
+        {
+            Decimal val;
+            if (Decimal.TryParse(source, NumberStyles.AllowDecimalPoint, lmBoxNumberFormat, out val))
+            {
+                return val;
+            }
+            else
+            {
+                throw new LmBoxException(String.Format("Expected value representing decimal for property '{0}', got '{1}'", propertyName, source));
+            }
+        }
+
+    }
+}
