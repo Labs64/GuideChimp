344d79a19ac8726d314249254225ae18f1879e27
R.Brown <ready.brown@hotmail.de>
2020-01-20 11:21:22 +0100

    Create _release.sh

---
+++ _release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# install dependencies
+npm install
+
+# check outdated dependencies
+npm outdated
+
+# execute tests
+npm run test
+
+# prepare distribution
+npm run build
+
+# version prompt
+echo Enter release version:
+read version
+if [ "$version" = "" ]; then
+  echo "Error! Version cannot be empty."
+  exit 1
+fi
+
+# start release
+git commit -a -m "Prepare for release $version"
+npm config set tag-version-prefix ''
+npm version $version -m "Release $version"
+git push origin master $version
