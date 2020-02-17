ffe0121f251d4fa31af235f4f301676bf1a42327
Slava Rudkovskiy <xraskolx@gmail.com>
2020-02-17 15:25:27 +0300

    remove url-loader

--- build/webpack.conf.js
+++ build/webpack.conf.js
@@ -48,27 +48,6 @@ const baseConfig = {
                     emitWarning: true,
                 },
             },
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                },
-            },
-            {
-                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                },
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                },
-            },
         ],
     },
     performance: { hints: false },
