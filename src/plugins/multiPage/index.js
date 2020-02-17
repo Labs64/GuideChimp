5017a05ad0b970090f931d148d99e97c3cf2ef4d
Alexey Averikhin <alexey.averikhin@labs64.de>
2020-02-17 13:11:35 +0100

    - add Labs64 copyright

--- src/plugins/multiPage/index.js
+++ src/plugins/multiPage/index.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (C) 2020 Labs64 GmbH
+ *
+ * This source code is licensed under the Apache License 2.0 license
+ * located in the LICENSE file and
+ * NOTICE file corresponding to the section 4 d of the Apache License, Version 2.0
+ */
+
 /* eslint-disable no-param-reassign,func-names */
 module.exports = (c) => {
     const storageKey = 'GUIDECHIMP_MULTIPAGE_STEP';
--- src/plugins/removeAttribution/index.js
+++ src/plugins/removeAttribution/index.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (C) 2020 Labs64 GmbH
+ *
+ * This source code is licensed under the Apache License 2.0 license
+ * located in the LICENSE file and
+ * NOTICE file corresponding to the section 4 d of the Apache License, Version 2.0
+ */
+
 module.exports = (cls) => {
     // eslint-disable-next-line no-param-reassign
     cls.prototype.showCopyright = () => document.createElement('div');
