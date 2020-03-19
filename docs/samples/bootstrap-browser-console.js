0c8a29e221531c466765d99e045a4ebb6efe2f9e
R.Brown <ready.brown@hotmail.de>
2020-03-19 10:38:21 +0100

    Create bootstrap-browser-console.js

---
+++ docs/samples/bootstrap-browser-console.js
@@ -0,0 +1,27 @@
+function loadScript(url, onLoad, location) {
+    var script = document.createElement('script');
+    script.src = url;
+    if (onLoad) {
+        script.onload = onLoad;
+    }
+    location = location || document.head;
+    location.appendChild(script);
+
+    return script;
+}
+
+function loadStyles(href, location) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = href;
+
+    location = location || document.head;
+    location.appendChild(link);
+    return link;
+}
+
+// load GuideChimp script
+loadScript('https://cdn.jsdelivr.net/npm/guidechimp@1/dist/guidechimp.min.js');
+// load GuideChimp styles
+loadStyles('https://cdn.jsdelivr.net/npm/guidechimp@1/dist/guidechimp.min.css');
