e48c16d53abec8dbd5600e912c24807ad107b5a2
R.Brown <ready.brown@hotmail.de>
2020-03-23 15:58:19 +0100

    Create bootstrap-plugings.js

---
+++ docs/samples/bootstrap-plugings.js
@@ -0,0 +1,14 @@
+function loadScript(url, onLoad, location) {
+    var script = document.createElement('script');
+    script.src = url;
+    if (onLoad) {
+        script.onload = onLoad;
+    }
+    location = location || document.head;
+    location.appendChild(script);
+
+    return script;
+}
+
+// load GuideChimp pluging
+loadScript('https://cdn.jsdelivr.net/npm/guidechimp@1/dist/plugins/removeAttribution.min.js');
