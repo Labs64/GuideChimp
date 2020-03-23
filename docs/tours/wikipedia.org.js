386176d4015400030f8b9e7763c83872274fe0f2
R.Brown <ready.brown@hotmail.de>
2020-03-23 14:27:40 +0100

    Create wikipedia.org.js
    Add wikipedia.org tour

---
+++ docs/tours/wikipedia.org.js
@@ -0,0 +1,22 @@
+var tour = [
+  {
+    element: '#mp-topbanner',
+    title: 'Welcome Banner',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    position: 'bottom',
+  },
+  {
+    element: '#mp-tfa',
+    title: 'Featured Article',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    position: 'right',
+  },
+  {
+    element: '#mp-itn',
+    title: 'In the news',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    position: 'left',
+  },
+];
+
+GuideChimp(tour).start();
