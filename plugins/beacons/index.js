7f3232f4dd53ed449edc35917db209967f94b158
Slava Rudkovskiy <xraskolx@gmail.com>
2020-07-17 14:23:50 +0300

    bug fix: beacons start tour without plugins included even though they were included

--- plugins/beacons/index.js
+++ plugins/beacons/index.js
@@ -5,11 +5,65 @@
  * located in the LICENSE file and
  * NOTICE file corresponding to the section 4 d of the Apache License, Version 2.0
  */
-
-import GuideChimpBeacons from './GuideChimpBeacons';
+import Beacons from './Beacons';
 import './assets/style.scss';
 
-module.exports = (cls, factory) => {
+module.exports = (Class, factory) => {
     // eslint-disable-next-line no-param-reassign
-    factory.beacons = (beacons, options = {}) => new GuideChimpBeacons(beacons, options);
+    factory.beacons = (beacons, options = {}) => new class extends Beacons {
+        getDataBeacons(ids) {
+            const data = super.getDataBeacons(ids);
+
+            data.forEach((beacon) => {
+                const { id, element: el } = beacon;
+
+                let tour = '';
+
+                const dataGlobalTourAttrName = `${this.constructor.getBeaconDataPrefix()}-tour`;
+                const dataBeaconTourAttrName = `${this.constructor.getBeaconDataPrefix()}-${id}-tour`;
+
+                if (el.attributes[dataGlobalTourAttrName]) {
+                    const { value } = el.attributes[dataGlobalTourAttrName];
+                    tour = value;
+                }
+
+                if (el.attributes[dataBeaconTourAttrName]) {
+                    const { value } = el.attributes[dataBeaconTourAttrName];
+                    tour = value;
+                }
+
+                if (tour) {
+                    // eslint-disable-next-line no-param-reassign
+                    beacon.tour = tour;
+                }
+            });
+
+            return beacons;
+        }
+
+        createBeaconEl(beacon) {
+            const beaconEl = super.createBeaconEl(beacon);
+
+            if (beacon.tour) {
+                beaconEl.addEventListener('click', () => {
+                    let guide = null;
+
+                    if (typeof beacon.tour === 'string' || Array.isArray(beacon.tour)) {
+                        guide = new Class(beacon.tour);
+                    } else if (beacon.tour instanceof Class) {
+                        guide = beacon.tour;
+                    } else if (typeof beacon.tour === 'object') {
+                        const { steps, options: tourOptions } = beacon.tour;
+                        guide = new Class(steps, tourOptions);
+                    }
+
+                    if (guide) {
+                        guide.start();
+                    }
+                });
+            }
+
+            return beaconEl;
+        }
+    }(beacons, options);
 };
